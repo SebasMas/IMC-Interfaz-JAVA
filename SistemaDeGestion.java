123c2ab8bb9917134724924c5a6b1e77d434bb42
Sebastián Diaz <99108952+SebasMas@users.noreply.github.com>
2023-04-19 20:24:29 -0500

    Add files via upload

---
+++ SistemaDeGestion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadegestion;
+
+/**
+ *
+ * @author Sebas
+ */
+public class SistemaDeGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
